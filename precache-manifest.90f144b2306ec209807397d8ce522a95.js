2837552327d1ffaf359df62802f55f328287771c
AlexCherny962 <53741736+AlexCherny962@users.noreply.github.com>
2020-07-20 23:41:11 +0300

    Updates

---
+++ precache-manifest.90f144b2306ec209807397d8ce522a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9183b8288f7c70c9105825cb1c6c68c0",
+    "url": "/cra6/index.html"
+  },
+  {
+    "revision": "cc5eb8d0fcd4a9ba888b",
+    "url": "/cra6/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aeddabc7e25374d22b9e",
+    "url": "/cra6/static/js/2.f6d1a848.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cra6/static/js/2.f6d1a848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5eb8d0fcd4a9ba888b",
+    "url": "/cra6/static/js/main.d0995b3d.chunk.js"
+  },
+  {
+    "revision": "aee38d4ca00157f484c7",
+    "url": "/cra6/static/js/runtime-main.dff2a71f.js"
+  }
+]);
